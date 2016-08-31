e741e9ce7b0631537a3b23b6821754c02a5414c2
garrett cullen <info@garrettcullen.com>
2016-08-30 22:00:19 -0700

    push

--- wp-content/themes/designerstouch/page-main.php
+++ wp-content/themes/designerstouch/page-main.php
@@ -5,6 +5,31 @@
 
 get_header(); ?>
 
+<div id="main">
+	
+	<div class="slideshow">
+		
+		<div class="slide">
+			
+			<div class="slide_content">
+				
+				<span>Welcome</span>
+				
+			</div><!-- slide_content -->
+			
+		</div><!-- slide -->
+		
+		
+	</div><!-- slideshow -->
+	
+	
+	
+	
+	
+</div><!-- main -->
+
+
+
 
 
 <?php get_footer(); ?>
