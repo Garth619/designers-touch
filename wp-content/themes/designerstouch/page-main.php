4653c7c3bf9a04d7560adb501978a6beaab9e9e2
garrett cullen <info@garrettcullen.com>
2016-09-04 16:02:20 -0700

    push

--- wp-content/themes/designerstouch/page-main.php
+++ wp-content/themes/designerstouch/page-main.php
@@ -15,7 +15,7 @@ get_header(); ?>
 				
 				<span class="slide_header">Slide Title Here</span><!-- slide_header -->
 				
-				<a href="" class="get_started">Get Started</a>
+				<a href="" class="blue_button">Get Started</a>
 				
 			</div><!-- slide_content -->
 			
@@ -86,7 +86,22 @@ get_header(); ?>
 			
 			<div class="about_slide">
 				
-				Hello
+				<div class="about_slide_content">
+				
+					<span class="large_header">About Designer's Touch</span><!-- large_header -->
+				
+					<p>Exciting Windows! by Designer’s Touch expert designer, Charles Marshall, has over 40 years of experience designing custom window treatments in Floyds Knobs, IN and Louisville, KY.</p> 
+
+					<p>Our head designer not only stays atop the window fashion trends, but he also creates them. His keen eye for detail and ability to capture your vision and make it into reality is what makes Exciting Windows! by Designer’s Touch a leader in the industry.</p>
+				
+				</div><!-- about_slide_content -->
+				
+				<div class="about_slide_image">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/charlie.jpg"/>
+					<span>Charlie Marshall</span>
+					
+				</div><!-- about_slide_image -->
 				
 				
 			</div><!-- about_slide -->
@@ -95,6 +110,96 @@ get_header(); ?>
 		
 		
 	</section><!-- about -->
+	
+	<section class="warranties">
+		
+		<span class="large_header">Four Remarkable Warranties</span><!-- large_header -->
+		
+		<a class="blue_button" href="">Learn More</a>
+		
+		<div class="four_warr_wrapper">
+			
+			<div class="single_warr">
+				
+				<span class="circle">1</span><!-- circle -->
+				
+				<span class="warr_description">The Best<br/>Price Anywhere</span><!-- warr_description -->
+				
+			</div><!-- single_warr -->
+			
+			<div class="single_warr">
+				
+				<span class="circle">2</span><!-- circle -->
+				
+				<span class="warr_description">The Best Quality<br/>and Workmanship</span><!-- warr_description -->
+				
+			</div><!-- single_warr -->
+			
+			<div class="single_warr">
+				
+				<span class="circle">3</span><!-- circle -->
+				
+				<span class="warr_description">A 10-Year<br/>Performance Assurance</span><!-- warr_description -->
+				
+			</div><!-- single_warr -->
+			
+			<div class="single_warr">
+				
+				<span class="circle">4</span><!-- circle -->
+				
+				<span class="warr_description">On-Time<br/>Appointments</span><!-- warr_description -->
+				
+			</div><!-- single_warr -->
+			
+		</div><!-- four_warr_wrapper -->
+		
+	</section><!-- warranties -->
+	
+	<section class="testimonials">
+		
+		
+		<span class="large_header">Client Testimonials</span><!-- large_header -->
+		
+		<div class="test_content">
+			
+			<span class="test">"This was the second time I have worked with Charlie.  He is great to work with.  He has a lot of experience.  He made suggestions and was very patient with me while deciding fabrics/ design.  He installed all of the products (blinds, shutters, and drapes) very well.  I had a small issue with the shutter; he resolved it quickly and without problem.  I would work with Charlie again and I would recommend him!"</span>
+			
+			<span class="name">Name of Person</span><!-- name -->
+			
+		</div><!-- test_content -->
+		
+		
+	</section><!-- testimonials -->
+	
+	<section class="leave_review">
+		
+		<span class="large_header">Leave a Review</span><!-- large_header -->
+		
+		<div class="review_icons">
+			
+			<a href="">
+				<img src="<?php bloginfo('template_directory');?>/images/bbb.png"/>
+			</a>
+			
+			<a href="">
+				<img src="<?php bloginfo('template_directory');?>/images/louis.png"/>
+			</a>
+			
+			<a href="">
+				<img src="<?php bloginfo('template_directory');?>/images/thumbtac.png"/>
+			</a>
+			
+			<a href="">
+				<img src="<?php bloginfo('template_directory');?>/images/al.png"/>
+			</a>
+			
+			<a href="">
+				<img src="<?php bloginfo('template_directory');?>/images/houzz.png"/>
+			</a>
+			
+		</div><!-- review_icons -->
+		
+	</section><!-- leave_review -->
 	
 	
 	
