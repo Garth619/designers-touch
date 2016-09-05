ce323e529ec4d77584b9cc73ae482065943fa9de
garrett cullen <info@garrettcullen.com>
2016-09-05 14:26:03 -0700

    push

--- wp-content/themes/designerstouch/banner.php
+++ wp-content/themes/designerstouch/banner.php
@@ -2,7 +2,16 @@
 		
 		<div class="inner_banner_content">
 			
-			<span><?php the_title();?></span>
+			<span class="large_header"><?php the_title();?></span>
+			
+			<div class="number_wrapper">
+				
+					<a class="numbers" href="tel:5022674971">Ky - 502 267 4971</a>
+					<a class="numbers" href="tel:8129442267">In - 812 944 2267</a>
+				
+				</div><!-- number_wrapper -->
+				
+				<a href="" class="blue_button">Get Started</a>
 			
 		</div><!-- inner_banner_content -->
 		
--- wp-content/themes/designerstouch/page.php
+++ wp-content/themes/designerstouch/page.php
@@ -17,47 +17,7 @@ get_header(); ?>
 		<div class="content">
 			
 			
-			
-<h1>Heading 1</h1>
-<h2>Heading 2</h2>
-<h3>Heading 3</h3>
-<h4>Heading 4</h4>
-<h5>Heading 5</h5>
-<h6>Heading 6</h6>
-<small><a href="#wrapper">[top]</a></small>
-
-
-
-<h2 id="paragraph">Paragraph</h2>
-<p>Lorem ipsum dolor sit amet, <a title="test link" href="#">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-<p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-<small><a href="#wrapper">[top]</a></small>
-
-
-
-<h2 id="list_types">List Types</h2>
-<h3>Definition List</h3>
-<dl><dt>Definition List Title</dt><dd>This is a definition list division.</dd></dl>
-<h3>Ordered List</h3>
-<ol>
-	<li>List Item 1</li>
-	<li>List Item 2</li>
-	<li>List Item 3</li>
-</ol>
-<h3>Unordered List</h3>
-<ul>
-	<li>List Item 1</li>
-	<li>List Item 2</li>
-	<li>List Item 3</li>
-</ul>
-<small><a href="#wrapper">[top]</a></small>
-
-
-
-			
-			
+			<?php get_template_part( 'loop', 'page' );?>
 			
 		</div><!-- content -->
 		
