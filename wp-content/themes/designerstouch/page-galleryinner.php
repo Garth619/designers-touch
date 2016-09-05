6c814930495f37b8286821c3acf0cdef9bbeff14
garrett cullen <info@garrettcullen.com>
2016-09-05 13:57:02 -0700

    push

---
+++ wp-content/themes/designerstouch/page-galleryinner.php
@@ -0,0 +1,127 @@
+<?php
+/**
+	
+	Template Name: Gallery Inner
+
+ */
+
+get_header(); ?>
+
+
+<div id="main">
+	
+	<?php include('banner.php');?>
+	
+	<div class="content_wrapper">
+		
+		<div class="gallery_wrapper">
+			
+			<a class="gallery_back" href="">Back to The Main Gallery Page</a>
+			
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+									
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+				
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+				
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+				
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+				
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			<div class="single_gallery_wrapper">
+				
+				<div class="gallery_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/gallery.jpg"/>
+				
+				</div><!-- img_wrapper -->
+				
+				<div class="gallery_content_wrapper">
+				
+					<span class="large_header">Arch Windows</span>
+				
+				</div><!-- content_wrapper -->
+			
+			</div><!-- single_gallery -->
+			
+			
+		</div><!-- gallery_wrapper -->
+				
+		
+	</div><!-- content_wrapper -->
+
+
+
+
+<?php get_footer(); ?>
